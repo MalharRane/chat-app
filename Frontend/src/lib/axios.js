bf3e39c9865a00abe123af454afcd56ffac847c8
MalharRane <malharane@gmail.com>
2026-03-20 01:45:53 +0530

    fixed crashing issue

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create(
-    {
-        baseURL:"http://localhost:5001/api",
-        withCredentials:true,
-    }
-)
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `${import.meta.env.VITE_BACKEND_URL}/api`,
+    withCredentials: true,
+});
